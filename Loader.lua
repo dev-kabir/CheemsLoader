309b5fcf69f70ea8ec080a52b673db7740a752a4
Raihan Kabir Choudhury <raihankabirchoudhury@gmail.com>
2026-05-18 02:08:07 +0530

    Implement HTTP GET function to load scripts

---
+++ Loader.lua
@@ -0,0 +1,62 @@
+local function HttpGet(url)
+
+    local curl =
+        Curl.Easy()
+
+    curl:Setopt(
+        eCurlOption.CURLOPT_URL,
+        url
+    )
+
+    curl:Perform()
+
+    local timeout =
+        os.clock() + 5
+
+    while not curl:GetFinished() do
+
+        if os.clock() > timeout then
+            return nil
+        end
+
+    end
+
+    local code, response =
+        curl:GetResponse()
+
+    return response
+end
+
+local SCRIPT_URL =
+"https://gist.githubusercontent.com/dev-kabir/2287bf40bcededc4c66edfa4cc42e239/raw/549a65b6fa360ca158a010f746d724281fe14da9/Main.lua"
+
+local script =
+    HttpGet(
+        SCRIPT_URL
+    )
+
+if script then
+
+    local func =
+        load(script)
+
+    if func then
+
+        local ok, err =
+            pcall(func)
+
+        if not ok then
+
+            GUI.AddToast(
+                "",
+                "Loader Error",
+                5000
+            )
+
+            Logger.LogError(err)
+
+        end
+
+    end
+
+end
